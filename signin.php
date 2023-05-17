70bdbb1f3e95804029259abda2787d3759f595ed
ntq2k3work <ntq2k3.work@gmail.com>
2023-05-17 17:43:24 +0700

    Form đăng nhập

---
+++ signin.php
@@ -0,0 +1,22 @@
+<div class="sign_form sign_in">
+    <div class="sign_form_header">
+        <p class="sign_form_title1">ĐĂNG NHẬP TÀI KHOẢN</p>
+        <p class="sign_form_title2">Nhập tài khoản và mật khẩu của bạn</p>
+    </div>
+    <hr style="opacity: 0.3; margin: 12px;">
+    <form action="process_signin.php" class="sign_form_input" method="post">
+        <input type="text" name="account" placeholder=" Nhập tài khoản hoặc email">
+        <input type="password" name="password"  placeholder=" Mật khẩu">
+        <div class="remember">
+            <input type="checkbox" name="remember"> Ghi nhớ đăng nhập
+        </div>
+        <div class="sign_policy">
+            This site is protected by reCAPTCHA and the <a href="https://policies.google.com/privacy" target="_blank"> Privacy Policy</a> and <a href="https://policies.google.com/terms" target="_blank">Terms of Service</a> apply.
+        </div>
+        <button class="sign_input_btn sign_in_btn">Đăng nhập</button>
+    </form>
+    <ul class="sign_help">
+        <li class="move_sign_up">Khách hàng mới?<a href="#">Tạo tài khoản</a></li>
+        <li class="move_forgot_pass">Quên mật khẩu?<a href="#">Khôi phục mật khẩu</a></li>
+    </ul>
+</div>
