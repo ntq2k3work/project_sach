65c1939a1ee5c1d6fa3c473ab789d17636c17f76
ntq2k3work <ntq2k3.work@gmail.com>
2023-05-17 17:47:58 +0700

    Gửi thông báo khi thao tác với user

--- modal_sign.php
+++ modal_sign.php
@@ -1,14 +1,21 @@
+
 <?php 
-        $error_sign ="";
-        if(isset($_GET['errorsi'])) $error_sign ="Tài khoản hoặc mật khẩu không chính xác!";
-        if(isset($_GET['errorsu'])) $error_sign ="Tài khoản hoặc email đã tồn tại !";
+        $notification ="";
+        if(isset($_SESSION['errorsi'])) $notification ="Tài khoản hoặc mật khẩu không chính xác!";
+        if(isset($_SESSION['errorsu'])) $notification ="Tài khoản hoặc email đã tồn tại !";
+        if(isset($_SESSION['successmail'])) $notification ="Email đã được gửi !";
 ?>
-<?php if($error_sign){?>
+<?php if(isset($_SESSION['errorsi']) || isset($_SESSION['errorsu']) || isset($_SESSION['successmail'])){?>
     <div id="modal" class="on_form">
         <div class="notify">
-            <div class="notify_content "><?php echo $error_sign  ?></div>
+            <div class="notify_content "><?php echo $notification  ?></div>
             <i class="fa-solid fa-xmark notify_icon"></i>
         </div>
     </div>
-<?php } ?>
-<script src="./assets/process_js/modal_sign.js"></script>
+<?php } 
+    unset($_SESSION['errorsu']);
+    unset($_SESSION['errorsi']);
+    unset($_SESSION['successmail']);
+
+?>
+    <script src="./assets/process_js/modal_sign.js"></script>
