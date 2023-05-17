7316da036437ae0c88b3c20039cd4fb0805f4ce3
ntq2k3work <ntq2k3.work@gmail.com>
2023-05-17 17:46:35 +0700

    Form khôi phục mật khẩu

---
+++ forgot.php
@@ -0,0 +1,18 @@
+<div class="sign_form forgot_pass">
+                        <div class="sign_form_header">
+                            <p class="sign_form_title1">KHÔI PHỤC MẬT KHẨU</p>
+                            <p class="sign_form_title2">Nhập tài khoản hoặc email</p>
+                        </div>
+                        <hr style="opacity: 0.3; margin: 12px;">
+                        <form action="process_forgot.php" class="sign_form_input" method="post" >
+                            <input type="text" name="email" placeholder=" Nhập tài khoản email đăng ký" required>
+                            <div class="sign_policy">
+                                Chúng tôi sẽ gửi mật khẩu tới email của bạn.Vui lòng kiểm tra hộp thư hoặc spam để lấy lại mật khẩu !
+                            </div>
+                            <button class="sign_input_btn sign_forgot_btn">Gửi email</button>
+                        </form>
+                        <ul class="sign_help">
+                            <li class="move_sign_in">Đã có tài khoản?<a href="#"> Đăng nhập</a></li>
+                            <li class="move_sign_up">Khách hàng mới?<a href="#"> Tạo tài khoản</a></li>
+                        </ul>
+                    </div>
